81fd9ab071b8670afa8a71dc8a574442899bfcfa
OhhDranoel <you@example.com>
2025-08-04 11:36:41 -0300

    grazadeus

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ com base nessas infomrações:
 Se você não encontrar a resposta para a pergunta do usuário nessas informações,
 responda não sei te dizer isso"""
 
-pergunta = input("Escreva sua pergunta: ")
+def perguntar():
+    pergunta = input("Escreva sua pergunta: ")
 
-# carregar banco de dados
-funcao_embedding = OpenAIEmbeddings()
-db = Chroma(persist_directory=CAMINHO_DB, embedding_function=funcao_embedding)
+    # carregar banco de dados
+    funcao_embedding = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CAMINHO_DB, embedding_function=funcao_embedding)
 
-# comparar a pergunta do usuário (embedding) com o meu banco de dados
-resultados = db.similarity_search_with_relevance_scores
+    # comparar a pergunta do usuário (embedding) com o meu banco de dados
+    resultados = db.similarity_search_with_relevance_scores(pergunta, k=3)
+    if len(resultados) == 0 or resultados [0][1] < 0.7:
+        print("Não conseguiu encontrar alguma infomração relevante na base")
+        return
